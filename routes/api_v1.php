48a6548d2729a50135b2b0665fe741ad15875ee6
TheVitik <lokplay2003@gmail.com>
2023-01-14 23:30:56 +0200

    Add API versioning

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\V1\CategoryController;
+use App\Http\Controllers\V1\RecordController;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('users', UserController::class)
+    ->only(['store']);
+Route::resource('categories', CategoryController::class)
+    ->only(['index', 'store']);
+Route::resource('records', RecordController::class)
+    ->only(['index', 'store']);
