02a69b0170764fdc57280ada2db358b565c0f2c1
TheVitik <lokplay2003@gmail.com>
2023-01-15 18:31:42 +0200

    Fix incorrect data displaying

--- app/Http/Resources/UserAccountResource.php
+++ app/Http/Resources/UserAccountResource.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Resources;
 
-use App\Models\V2\Account;
+use App\Models\V2\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class UserAccountResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'account' => new UserAccountResource(Account::where('user_id', $this->id)->first())
+            'balance' => $this->balance
         ];
     }
 }
